3e426146cfa4b45f38d081d16f49a08211abd616
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2023-07-29 21:01:13 -0600

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Marvin Manuel Menchú Menchú on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
