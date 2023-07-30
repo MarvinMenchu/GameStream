789157698e0fe12895a8c9ad8bf998da0a91ff0c
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2023-07-29 21:14:54 -0600

    CREAMOS EL PROYECTO GAMESTREAM

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -9,18 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+
+            ZStack {
+                Color(.blue)
+            }
+
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Image("pantalla01").resizable()
         ContentView()
+        
     }
 }
